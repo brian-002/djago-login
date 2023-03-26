bee4c0652f2e2cb458ae489bf2c806a6f1d94419
Brian Kiptoo <77725948+brian-002@users.noreply.github.com>
2023-03-26 21:05:52 +0300

    Update info.py

--- gfg/info.py
+++ gfg/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS ="True"
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'djagologindemo@gmail.com'
-EMAIL_HOST_PASSWORD = '1q2w-3e-4r5t'
+EMAIL_HOST_USER = 'djangologin@gmail.com'
+EMAIL_HOST_PASSWORD = 'pass'
 EMAIL_PORT = 587
